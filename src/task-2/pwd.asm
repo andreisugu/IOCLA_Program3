7427b566e7e808e346aecc9698bd3824bbe310f0
andreisugu <35407471+andreisugu@users.noreply.github.com>
2023-05-22 07:49:52 +0000

    TASK-2 => facut

--- src/task-2/pwd.asm
+++ src/task-2/pwd.asm
@@ -13,6 +13,7 @@ section .text
 ;;	void pwd(char **directories, int n, char *output)
 ;	Adauga in parametrul output path-ul rezultat din
 ;	parcurgerea celor n foldere din directories
+
 pwd:
 	enter 0, 0
 	pusha
@@ -21,6 +22,8 @@ pwd:
 	mov ebx, dword[ebp + 12] 	; n
 	mov edx, dword[ebp + 16] 	; output
 
+
+; ACTIVE CODE
 	; we go through each directory
 	; for each directory, we add to output "/" and the name of the directory
 	; we will use ecx to go through the directories
@@ -32,77 +35,95 @@ first_loop:
 	
 	; we check for "." and ".."
 	; if we find them, we modify the directories array to reflect the changes
-	cmp dword[eax + ecx * 4], curr
-	je curr_dir
-	cmp dword[eax + ecx * 4], back
-	je back_dir
-
-	; because its neither "." nor "..", we simply continue
+	; save ebx
+	push ebx
+	xor ebx, ebx
+	; we check if the directory is "." or ".."
+	mov esi, [eax + ecx * 4]
+	mov bl, byte[esi]
+	cmp bl,  '.'
+	je action_decider
+	; restore ebx
+	pop ebx
+	; because its neither "." nor "..", we add the name of the directory to output
+	; we also add "/" to output
+	mov byte[edx], '/'
+	inc edx
+	; we add the name of the directory to output
+	; we will use esi to go through the name
+	mov esi, dword[eax + ecx * 4]
+	; save ecx
+	push ecx
+	xor ecx, ecx
+	second_loop:
+		; if we finished iterating through the name, exit
+		cmp byte[esi], 0
+		je end_second_loop
+	
+		; we copy the character to output
+		mov cl, byte[esi]
+		mov byte[edx], cl
+		inc edx
+		inc esi
+		jmp second_loop
+	end_second_loop:
+	; we continue
+	; restore ecx
+	pop ecx
+continue_first_loop:
 	inc ecx
 	jmp first_loop
 
+action_decider:
+	mov bl, byte[esi + 1]
+	cmp bl, '.'
+	je back_dir
+	jmp curr_dir
+
 curr_dir:
-	; because its the current dir, we move the entire eax array one position to the left
-	; we also decrement ebx
-	mov edi, eax
-	mov esi, eax
-	add esi, 4
-	mov ecx, ebx
-	sub ecx, 1
-	rep movsd
-	dec ebx
-	jmp first_loop
+	; dont do anything
+	; restore ebx
+	pop ebx
+	jmp continue_first_loop
 
 back_dir:
 	; because its the parent dir, we move the entire eax array two positions to the left
-	; we also decrement ebx twice
-	mov edi, eax
-	mov esi, eax
-	add esi, 8
-	mov ecx, ebx
-	sub ecx, 2
-	rep movsd
-	sub ebx, 2
-	jmp first_loop
+	; restore ebx
+	pop ebx
+	jmp move_left
 
+move_left:
+	; we check if we can move anymore left
+	cmp edx, dword[ebp + 16]
+	je continue_first_loop
+	; we check if we have reached '/'
+	push ebx
+	xor ebx, ebx
+	mov bl, byte[edx]
+	cmp bl, '/'
+	je end_move_left
+	pop ebx
+	mov byte[edx], 0
+	dec edx
+	jmp move_left
+
+end_move_left:
+	pop ebx
+	; also eliminte the '/'
+	mov byte[edx], 0
+	; we continue
+	jmp continue_first_loop
+ 
 	; we finished iterating through the directories
 end_first_loop:
-	; now we iterate again and simply add the names of the directories to output with "/"
-	; we will use ecx to go through the directories
-	xor ecx, ecx
-second_loop:
-	; if we finished iterating through the directories, exit
-	cmp ecx, ebx
-	je end_second_loop
-
-	; we add "/" to output
+; END ACTIVE CODE
+	; add a final "/"
 	mov byte[edx], '/'
 	inc edx
 
-	; we add the name of the directory to output
-	; we copy each character of the name to output
-	; we will use esi to go through the name
-	mov esi, dword[eax + ecx * 4]
-third_loop:
-	; if we finished iterating through the name, exit
-	cmp byte[esi], 0
-	je end_third_loop
-
-	; we copy the character to output
-	mov al, byte[esi]
-	mov byte[edx], al
-	inc edx
-	inc esi
-	jmp third_loop
-
-end_third_loop:
-
-
-	; we increment ecx and continue
-	inc ecx
-	jmp second_loop
-
-end_second_loop:
+	;mov ebx, [eax + 16] ; alegem propozitia (iteratii de 4)
+	;mov cl, byte[ebx + 2]  ; copiem caracterul din propozitie pe care il dorim (iteratii de 4)
+	;mov byte[edx], cl  ; introducem caracterul in rezultat
 
 	popa
 	leave
